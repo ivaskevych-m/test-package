d4232e266d3ff8f10e5ff742b528237bc13910e4
geraltIV <myroslav.ivaskevych92@gmail.com>
2023-09-20 14:39:31 +0300

    workflow to publish package

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello, World!");
+import { createRoot } from 'react-dom/client';
+
+document.body.innerHTML = '<div id="app"></div>';
+
+const root = createRoot(document.getElementById('app'));
+
+root.render(<h1>Hello, world</h1>);
