636150e79c72565bd540c26c98f85a3f55db4244
geraltIV <myroslav.ivaskevych92@gmail.com>
2023-09-20 15:04:16 +0300

    Updated

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-import { createRoot } from 'react-dom/client';
-
-document.body.innerHTML = '<div id="app"></div>';
-
-const root = createRoot(document.getElementById('app'));
-
-root.render(<h1>Hello, world</h1>);
+console.log('Hello World!');
